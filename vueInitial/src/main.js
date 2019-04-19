e01cc1560c4a566ae6240765d26601bc8a16c502
雾山 <li997477295@outlook.com>
2019-04-19 16:36:13 +0800

    react初始化

--- vueInitial/src/main.js
+++ vueInitial/src/main.js
@@ -4,23 +4,26 @@ import Router from './router'
 import Store from './store'
 import i18n from './locales'
 
-//  全局样式
+//  引入 全局样式
 import './assets/styles/global.scss'
 import './assets/iconfonts/iconfont.css'
-// import 'swiper/dist/css/swiper.css'
 
-// 引入elementUI
+// 引入 swiper
+import 'swiper/dist/css/swiper.css'
+
+// 引入 elementUI
 import './plugins/element.js'
 import 'element-ui/lib/theme-chalk/display.css'
 
-// 引入rem插件(适用非响应式布局,如要兼容pc，设置node_modules/lib-flexible/文件里72行为 54->width)
+// 引入 rem插件(适用非响应式布局,如要兼容pc，设置node_modules/lib-flexible/文件里72行为 54->width)
 import 'lib-flexible/flexible.js'
 
-// 引入svg处理
+// 引入 svg处理
 import './assets/icons'
 
-// 引入请求方法,倒入 http 文件夹下的 index.js
+// 引入 请求方法,倒入 http 文件夹下的 index.js
 import api from './utils/http/index'
+
 Vue.use(api)
 Vue.config.productionTip = false
 
